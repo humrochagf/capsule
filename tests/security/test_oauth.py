8d6509e573e3a5643f349e9567a3be90c045ffba
Humberto Rocha <humrochagf@gmail.com>
2024-11-01 22:47:36 -0400

    Auth tests

--- tests/security/test_oauth.py
+++ tests/security/test_oauth.py
@@ -1,6 +1,7 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 from httpx import BasicAuth
+from pydantic_core import Url
 
 from capsule.settings import CapsuleSettings
 
@@ -283,3 +284,106 @@ def test_token_client_credentials(client: TestClient) -> None:
     assert data["access_token"] == "__app__"
     assert data["token_type"] == "Bearer"
     assert data["scope"] == "read"
+
+
+def test_token_authorization_code(
+    client: TestClient,
+    capsule_settings: CapsuleSettings,
+) -> None:
+    payload = {
+        "client_name": "Token Authorization Code Test",
+        "redirect_uris": "https://example.com",
+    }
+
+    response = client.post("/api/v1/apps", json=payload)
+    assert response.status_code == status.HTTP_200_OK
+
+    app = response.json()
+    auth = BasicAuth(username=capsule_settings.username, password="p4ssw0rd")
+    payload = {
+        "client_id": app["client_id"],
+        "scope": app["scopes"],
+        "redirect_uri": app["redirect_uris"],
+        "response_type": "code",
+    }
+
+    response = client.post(
+        "/oauth/authorize", auth=auth, json=payload, follow_redirects=False
+    )
+    assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+
+    code = ""
+    for query_item in (Url(response.headers["location"]).query or "").split("&"):
+        key, value = query_item.split("=")
+
+        if key == "code":
+            code = value
+
+    payload = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "client_id": app["client_id"],
+        "client_secret": app["client_secret"],
+        "redirect_uri": "https://example.com",
+    }
+
+    response = client.post("/oauth/token", json=payload)
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_token_authorization_code_no_app(client: TestClient) -> None:
+    payload = {
+        "grant_type": "authorization_code",
+        "code": "no app",
+        "client_id": "no client",
+        "client_secret": "no secret",
+        "redirect_uri": "https://example.com",
+    }
+
+    response = client.post("/oauth/token", json=payload)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_token_authorization_code_bad_redirect_uri(
+    client: TestClient,
+    capsule_settings: CapsuleSettings,
+) -> None:
+    payload = {
+        "client_name": "Token Authorization Code bad redirect Test",
+        "redirect_uris": "https://example.com",
+    }
+
+    response = client.post("/api/v1/apps", json=payload)
+    assert response.status_code == status.HTTP_200_OK
+
+    app = response.json()
+    auth = BasicAuth(username=capsule_settings.username, password="p4ssw0rd")
+    payload = {
+        "client_id": app["client_id"],
+        "scope": app["scopes"],
+        "redirect_uri": app["redirect_uris"],
+        "response_type": "code",
+    }
+
+    response = client.post(
+        "/oauth/authorize", auth=auth, json=payload, follow_redirects=False
+    )
+    assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+
+    code = ""
+    for query_item in (Url(response.headers["location"]).query or "").split("&"):
+        key, value = query_item.split("=")
+
+        if key == "code":
+            code = value
+
+    payload = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "client_id": app["client_id"],
+        "client_secret": app["client_secret"],
+        "redirect_uri": "https://bad-redirect.com",
+    }
+
+    response = client.post("/oauth/token", json=payload)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
